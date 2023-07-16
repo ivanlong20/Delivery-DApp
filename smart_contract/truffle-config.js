b757f6279921885d04e08688cb516a5bb368b28a
ivanlong20 <ivan.kaalong@gmail.com>
2023-07-16 19:08:53 +0800

    submission

---
+++ smart_contract/truffle-config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  networks: {
+    loc_development_development: {
+      network_id: "*",
+      port: 8545,
+      host: "127.0.0.1"
+    }
+  },
+  mocha: {},
+  compilers: {
+    solc: {
+      version: "0.8.19",
+      settings: {
+        optimizer: {
+          enabled: false,
+          runs: 2000
+        },
+        viaIR: true
+      }
+    }
+  }
+};
